3be4c38262dd0d68ac54f6dd7faa403b7b9c387d
Sai Uttarkar <usai7261@gmail.com>
2024-04-24 08:49:54 +0530

    shortened the code

--- poc2/popup.js
+++ poc2/popup.js
@@ -3,13 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
 	const titleDisplay = document.getElementById("title-display");
 
 	button.addEventListener("click", function () {
-		chrome.tabs.query(
-			{ active: true, currentWindow: true },
-			function (tabs) {
-				const currentTab = tabs[0];
+		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+			const currentTab = tabs[0];
 
-				titleDisplay.innerHTML = currentTab.title;
-			},
-		);
+			titleDisplay.innerHTML = currentTab.title;
+		});
 	});
 });
